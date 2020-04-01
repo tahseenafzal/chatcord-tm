9a0ab16226279432f3f9577b918d23e3acf83f01
Tehseen Afzal <abuashja@gmail.com>
2020-04-01 21:58:03 +0500

    create settings for messages in socket io connection

--- server.js
+++ server.js
@@ -12,7 +12,16 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Run when client connects
 io.on("connection", socket => {
-  console.log(`New WS Connection...`);
+  // Welcome current use
+  socket.emit("message", "Welcome to ChatCord!");
+
+  // Broadcast when a user connects
+  socket.broadcast.emit("message", "A user has joined the chat.");
+
+  // Run when client disconnets
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left the chat.");
+  });
 });
 
 const PORT = 3000 || process.env.PORT;
